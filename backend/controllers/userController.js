ca3ade9e27a51f2bd7f136b3582193ee409e8761
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-02-05 23:00:48 +0300

    adding get individual user and get all users api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,3 +33,27 @@ export const createUser = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error });
+    console.log(error);
+  }
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) return res.status(404).json({ error: "Users not found" });
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error });
+    console.log(error);
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getAllUsers,
+  getUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post(`/`, createUser);
+router.get(`/:id`, getUser);
+router.get(`/`, getAllUsers);
 
 export default router;
